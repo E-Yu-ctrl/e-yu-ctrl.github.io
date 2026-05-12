005c5a428c80cb98cd3bb149e6c6bcb5b003f228
Emma Yu <eyu26994@gmail.com>
2026-05-12 12:30:12 -0700

    Update project repository name in config

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['tvanlaar/biol4810',], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['E-Yu-ctrl/biol4810',], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
